e0490d0126557f5c996fbf91d45913fa0184a8c2
BuildTools <unconfigured@null.spigotmc.org>
2020-06-13 21:42:41 +0200

    updating game.h commentaries

--- include/game/game.h
+++ include/game/game.h
@@ -18,12 +18,42 @@ typedef struct {
 //to register the user's win in the leaderboard
 int gameLoop(SDL_Surface *screen, SDL_Surface *background, char *player_name);//The entire game
 
-char startPasses(Card_t * deck_of_32_cards, Card_t all_player_and_AI_cards[4][8], Player_t * player_and_AI, SDL_Surface *screen, SDL_Surface *background); //function that start the passes and return the trump color
+/*Function that start passes. It consists of a loop that will iterate trough player and AIs and ask them to choose a bet and a trump
+  until 3 players skipped after the last bet.
 
-void startRound(Player_t * player_and_AI, Card_t all_player_and_AI_cards[4][8], char trump, SDL_Surface *screen, SDL_Surface *background); //start a round with 8 tricks
+  Arguments description :
+  - all_player_and_AI_cards : array that contain player's and AIs hand
+  - player_and_AI array : that contain each player and AIs
+  - screen : the screen surface to display with SDL
+  - background : screen's background
 
+  This function return a char containing the trump color (or all trumps / no trumps) for the round*/
+char startPasses(Card_t all_player_and_AI_cards[4][8], Player_t * player_and_AI, SDL_Surface *screen, SDL_Surface *background);
+
+
+/*Function that start the round. it consists of a for loop that iterates 8 tricks where we will get the played card input for each
+  Player and AIs and put them on the table. Then get the winner add points to it's point property, belote, xdder.
+
+  Arguments description:
+  - player_and_AI : array containing each player and AIs
+  - all_player_and_AI_cards : array containing player's and AIs hand
+  - trump : char that contain the trump color
+  - screen : the screen surface to display with SDL
+  - background : screen's background*/
+void startRound(Player_t * player_and_AI, Card_t all_player_and_AI_cards[4][8], char trump, SDL_Surface *screen, SDL_Surface *background);
+
+
+/*Function that shift the array containing player and AIs and sets the corresponding index first
+
+  Arguments description :
+  - player_and_AI : array containing each player and AI
+  - index : index of the player / AI that we want to put at first*/
 void changePlayerOrder(Player_t * player_and_AI, int index); //move all players to the left until the one selected is in first place
 
+/*Function that initialize players and AI. It sets their ID and every other value to 0
+
+  Arguments description:
+  - player_and_AI : array containg each player and AI*/
 void initializePlayers(Player_t * player_and_AI); //variables for every player to 0
 
 //Called at the end of each round, adds up players points in to teams points.
