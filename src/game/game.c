a0e29d9c47c023e87ca1434b5b3541cb699f94d3
BuildTools <unconfigured@null.spigotmc.org>
2020-06-13 12:14:22 +0200

    removing for loop initial declarations

--- src/game/game.c
+++ src/game/game.c
@@ -410,7 +410,8 @@ void points_end_round(Player_t players[], int points_teams[])
 {
   int bet = 0;
   int team_bet_id;
-  for (int i = 0; i < 4; i++)
+  int i;
+  for (i = 0; i < 4; i++)
   {
     if(players[i].bet > bet)
     {
@@ -419,7 +420,7 @@ void points_end_round(Player_t players[], int points_teams[])
     }
   }
 
-  for (int i=0; i<2; i++)
+  for (i = 0; i<2; i++)
   {
       if (i==team_bet_id)
       {
